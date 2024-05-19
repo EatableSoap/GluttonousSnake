edcc08ce961093687be8801ec337933a9f3e4006
agoodgamer <2646319600@qq.com>
2024-05-19 22:10:28 +0800

    1

--- Snake/finger_recognization.py
+++ Snake/finger_recognization.py
@@ -1,2 +1,36 @@
-import numpy
-from PIL import Image
+import cv2 as cv
+from PIL import ImageTk, Image
+from pynput import keyboard
+
+from snake_class import Snake
+
+ip = "10.198.146.150"
+port = "4747"
+url = "http://" + ip + ":" + port + "/video"
+
+cap = cv.VideoCapture(url)
+game = Snake(Fps=100)
+control=keyboard.Controller()
+# pos = game.set_in_windows()
+
+
+def printpic(local_game):
+    ret, frame = cap.read()
+    img=frame
+    # cv.moveWindow("", int(pos[0]), int(pos[1]))
+    if ret:
+        img = cv.resize(img, (local_game.Width, local_game.Height), interpolation=cv.INTER_NEAREST)
+        # img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        cv.imshow("Phone Camera", img)
+        if local_game.game_loop():
+            local_game.win.after(0, lambda: printpic(local_game))
+        else:
+
+    else:
+        return
+
+
+printpic(game)
+game.win.mainloop()
+cap.release()
+cv.destroyAllWindows()
--- Snake/snake_class.py
+++ Snake/snake_class.py
@@ -9,14 +9,17 @@ class Snake:
         y1 = row * self.Unit_size
         x2 = (col + 1) * self.Unit_size
         y2 = (row + 1) * self.Unit_size
-        can.create_rectangle(x1, y1, x2, y2, fill=unit_color, outline="Dimgray")
+        can.create_rectangle(x1, y1, x2, y2, fill=unit_color, outline="")
 
     # 绘制背景
-    def put_a_background(self, can, color='Dimgray'):
+    def put_a_background(self, can, color='white'):
         for x in range(self.Column):
             for y in range(self.Row):
                 self.draw_a_unit(can, x, y, unit_color=color)
                 self.game_map.append([x, y])
+
+    def set_pic(self, pic=None):
+        return self.canvas.create_image(0, 0, anchor=tk.NW, image=pic)
 
     # 绘制蛇
     def draw_the_snake(self, can, snake, color='green'):
@@ -28,37 +31,38 @@ class Snake:
 
     # 判断方向
     def find_dirction(self, event):
-        # 判断是否可以向上向下操作
-        # 如果snake_list[0] 和 [1] 的x轴坐标相同，意味着不可以改变上、下方向
-        # 若y轴坐标相同，意味着不可以改变左、右方向
-        ch = event.keysym
-        delta_x = self.Dirc[0]
-        delta_y = self.Dirc[1]
-        if ch == 'w':
-            if self.snake_list[0][0] != self.snake_list[1][0]:  # or snake_list[0][1] < snake_list[1][1]:
-                delta_x = 0
-                delta_y = -1
-            if self.snake_list[0][1] < self.snake_list[1][1]:
-                self.move_snake(self.snake_list, [delta_x, delta_y], True)
-        elif ch == 's':
-            if self.snake_list[0][0] != self.snake_list[1][0]:  # or snake_list[0][1] > snake_list[1][1]:
-                delta_x = 0
-                delta_y = 1
-            if self.snake_list[0][1] > self.snake_list[1][1]:
-                self.move_snake(self.snake_list, [delta_x, delta_y], True)
-        elif ch == 'a':
-            if self.snake_list[0][1] != self.snake_list[1][1]:  # or self.snake_list[0][0] < self.snake_list[1][0]:
-                delta_y = 0
-                delta_x = -1
-            if self.snake_list[0][0] < self.snake_list[1][0]:
-                self.move_snake(self.snake_list, [delta_x, delta_y], True)
-        elif ch == 'd':
-            if self.snake_list[0][1] != self.snake_list[1][1]:  # or self.snake_list[0][0] > self.snake_list[1][0]:
-                delta_y = 0
-                delta_x = 1
-            if self.snake_list[0][0] > self.snake_list[1][0]:
-                self.move_snake(self.snake_list, [delta_x, delta_y], True)
-        self.Dirc = [delta_x, delta_y]
+        if not self.pause_flag == 1:
+            # 判断是否可以向上向下操作
+            # 如果snake_list[0] 和 [1] 的x轴坐标相同，意味着不可以改变上、下方向
+            # 若y轴坐标相同，意味着不可以改变左、右方向
+            ch = event.keysym
+            delta_x = self.Dirc[0]
+            delta_y = self.Dirc[1]
+            if ch == 'w':
+                if self.snake_list[0][0] != self.snake_list[1][0]:  # or snake_list[0][1] < snake_list[1][1]:
+                    delta_x = 0
+                    delta_y = -1
+                if self.snake_list[0][1] < self.snake_list[1][1]:
+                    self.move_snake(self.snake_list, [delta_x, delta_y], True)
+            elif ch == 's':
+                if self.snake_list[0][0] != self.snake_list[1][0]:  # or snake_list[0][1] > snake_list[1][1]:
+                    delta_x = 0
+                    delta_y = 1
+                if self.snake_list[0][1] > self.snake_list[1][1]:
+                    self.move_snake(self.snake_list, [delta_x, delta_y], True)
+            elif ch == 'a':
+                if self.snake_list[0][1] != self.snake_list[1][1]:  # or self.snake_list[0][0] < self.snake_list[1][0]:
+                    delta_y = 0
+                    delta_x = -1
+                if self.snake_list[0][0] < self.snake_list[1][0]:
+                    self.move_snake(self.snake_list, [delta_x, delta_y], True)
+            elif ch == 'd':
+                if self.snake_list[0][1] != self.snake_list[1][1]:  # or self.snake_list[0][0] > self.snake_list[1][0]:
+                    delta_y = 0
+                    delta_x = 1
+                if self.snake_list[0][0] > self.snake_list[1][0]:
+                    self.move_snake(self.snake_list, [delta_x, delta_y], True)
+            self.Dirc = [delta_x, delta_y]
         return
 
     # 生成食物,判断是否有食物
@@ -91,7 +95,7 @@ class Snake:
             if not self.snake_eat(snke_list, self.Food_pos):
                 self.Energy -= 1
                 self.str_energy.set('Energy:' + str(self.Energy))
-                self.draw_a_unit(self.canvas, snke_list[-1][0], snke_list[-1][1], unit_color="Dimgray")
+                self.draw_a_unit(self.canvas, snke_list[-1][0], snke_list[-1][1], unit_color="white")
                 snke_list.pop(-1)
                 self.draw_a_unit(self.canvas, snke_list[1][0], snke_list[1][1])
                 self.draw_a_unit(self.canvas, temp_head[0], temp_head[1], unit_color="black")
@@ -116,6 +120,7 @@ class Snake:
         set_list = snke_list[1:]
         # 判断头和身体是否重叠或判断超出边界
         if snke_list[0] in set_list or x < 0 or x > self.Column - 1 or y < 0 or y > self.Row - 1 or self.Energy <= 0:
+            # self.pause_flag = 1
             return 1
         else:
             return 0
@@ -138,16 +143,18 @@ class Snake:
         if self.game_over(self.snake_list):
             self.over_label = tk.Label(self.win, text='Game Over', font=('楷体', 25), width=15, height=1)
             self.over_label.place(x=(self.Width - 260) / 2, y=(self.Height - 40) / 2, bg=None)
-            return
-        if self.pause_flag == -1:
-            self.win.after(self.Fps, self.game_loop)
+            return False
         else:
-            second = tk.Tk()
-            pause_button = tk.Button(second, width=20, height=1, text="Continue",
-                                     command=lambda: self.continue_game(second),
-                                     relief='raised')
-            pause_button.pack_configure(expand=1)
-            second.geometry("%dx%d+%d+%d" % (20, 30, 400, 400))
+            return True
+        # elif self.pause_flag == -1:
+        #     self.win.after(self.Fps, self.game_loop)
+        # else:
+        #     second = tk.Tk()
+        #     pause_button = tk.Button(second, width=20, height=1, text="Continue",
+        #                              command=lambda: self.continue_game(second),
+        #                              relief='raised')
+        #     pause_button.pack_configure(expand=1)
+        #     second.geometry("%dx%d+%d+%d" % (20, 30, 400, 400))
 
     def key_bind(self, canvas):
         canvas.focus_set()
@@ -159,9 +166,10 @@ class Snake:
         canvas.bind("<KeyPress-Escape>", self.Exit_game)
         canvas.bind("<KeyPress-space>", self.Pause_game)
 
-    def Restart_game(self, event):
+    def Restart_game(self,event=None):
         if not self.over_label is int:
             self.over_label.destroy()
+        self.pause_flag = -1
         self.Dirc = [0, 0]
         self.Score = 0
         self.Energy = 400
@@ -173,8 +181,7 @@ class Snake:
         self.draw_the_snake(self.canvas, self.snake_list)
         self.setlable()
         self.key_bind(self.canvas)
-        self.game_loop()
-        self.win.mainloop()
+
 
     def ramdom_snake(self):
         # 随机生成蛇的位置
@@ -189,7 +196,15 @@ class Snake:
         self.str_energy.set('Energy:' + str(self.Energy))
         self.str_time.set("Time:" + str(self.Time))
 
-    def __init__(self, row=40, column=40,Fps=100,Unit_size=20):
+    def set_in_windows(self):
+        screenWidth = self.win.winfo_screenwidth()  # 获取显示区域的宽度
+        screenHeight = self.win.winfo_screenheight()  # 获取显示区域的高度
+        left = (screenWidth - self.Width) / 2
+        top = (screenHeight - (self.Height + 2 * self.Unit_size)) / 2
+        self.win.geometry("%dx%d+%d+%d" % (self.Width, self.Height, left, top))
+        return left, top
+
+    def __init__(self, row=40, column=40, Fps=100, Unit_size=20):
         self.Fps = Fps
         self.pause_flag = -1
         self.over_label = 1
@@ -199,6 +214,7 @@ class Snake:
         self.Height = row * self.Unit_size
         self.Width = column * self.Unit_size
         self.win = tk.Tk()
+        self.win.attributes("-transparentcolor", "white")
         self.win.title("Snake")
         self.canvas = tk.Canvas(self.win, width=self.Width, height=self.Height + 2 * self.Unit_size)
         self.canvas.pack()
@@ -216,16 +232,17 @@ class Snake:
         self.str_energy = tk.StringVar()
         self.str_time = tk.StringVar()
         self.score_label = tk.Label(self.win, textvariable=self.str_score, font=('楷体', Unit_size), width=10, height=1)
-        self.energy_label = tk.Label(self.win, textvariable=self.str_energy, font=('楷体', Unit_size), width=10, height=1)
+        self.energy_label = tk.Label(self.win, textvariable=self.str_energy, font=('楷体', Unit_size), width=10,
+                                     height=1)
         self.time_label = tk.Label(self.win, textvariable=self.str_time, font=('楷体', Unit_size), width=10, height=1)
         self.setlable()
         self.score_label.place(x=(self.Width - 400) / 2, y=self.Height)
         self.energy_label.place(x=(self.Width - 100) / 2, y=self.Height)
         self.time_label.place(x=(self.Width + 200) / 2, y=self.Height)
+        # self.set_in_windows()
         # 绑定按键
         self.key_bind(self.canvas)
 
-
-game = Snake(Unit_size=20)
-game.game_loop()
-game.win.mainloop()
+# game = Snake(Unit_size=20)
+# game.game_loop()
+# game.win.mainloop()
